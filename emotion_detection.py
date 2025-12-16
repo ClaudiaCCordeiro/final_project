7e342373524b029f0a4fb5555bdf72dee8a90542
Claudia Regina Carvalho Cordeiro Silva <claudiaccordeiro@gmail.com>
2025-12-16 12:20:53 -0300

    atualizado

--- emotion_detection.py
+++ emotion_detection.py
@@ -4,4 +4,4 @@ def emotion_detector(text_to_analyse):  # Define a function named emotion_detect
     myobj = { "raw_document": { "text": text_to_analyse } }  # Create a dictionary with the text to be analyzed
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}  # Set the headers required for the API request
     response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
-    return response.text  # Return the response text from the API
+    return response.text  # Return the response text from the APIc
